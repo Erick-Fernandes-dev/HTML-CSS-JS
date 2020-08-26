0272914705514c887d423faed64b291b161080e8
Erick Fernandes <56647805+TBWA0002@users.noreply.github.com>
2020-08-26 15:05:56 -0300

    chore: Tutorial de HTML e CSS e um pouco de JS, apenas o básico

---
+++ java/treino01.java
@@ -0,0 +1,14 @@
+public class treino01 {
+    public static void main(String[] args){
+        String nome = "Erick";
+        int idade = 19;
+        if (idade < 12) {
+            System.out.println("Você é uma criança.");
+            
+        } else {
+            System.out.println("Você é Adulto.");
+            
+        }
+        System.out.println(nome);
+    }
+}
